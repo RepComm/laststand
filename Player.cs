e3dcea3af94c6d68f35265991d41fbbaf0fb7952
Jon Crowder <rccommandotac@gmail.com>
2024-01-29 21:50:54 -0600

    some changes to input

--- Player.cs
+++ Player.cs
@@ -9,6 +9,29 @@ public partial class Player : CharacterBody3D
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
+  Vector2 look;
+
+  [Export]
+  float lookSensitivity = 0.005f;
+
+  public override void _Input(InputEvent @event) {
+    if (@event is InputEventMouseButton emb) {
+      if (Input.MouseMode != Input.MouseModeEnum.Captured) {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+      }
+    } else if (@event is InputEventMouseMotion emm) {
+      if (Input.MouseMode == Input.MouseModeEnum.Captured) {
+        look.X = emm.Relative.X;
+        look.Y = emm.Relative.Y;
+      }
+    }
+  }
+  public override void _Process(double delta) {
+    if (Input.IsActionPressed("MouseReleaseCapture")) {
+      Input.MouseMode = Input.MouseModeEnum.Visible;
+    }
+  }
+
 	public override void _PhysicsProcess(double delta) {
 		Vector3 velocity = Velocity;
 
@@ -17,20 +40,17 @@ public partial class Player : CharacterBody3D
 			velocity.Y -= gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("strafe_left", "strafe_right", "fwd", "bwd");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
+		if (direction != Vector3.Zero) {
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
+		} else {
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
